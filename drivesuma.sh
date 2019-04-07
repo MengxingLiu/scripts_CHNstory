337cb594b12a4e974fb279f5b372bd4d2f76a815
MENGXING <mengluo1990@gmail.com>
2019-04-07 18:51:18 +0200

    save suma image

---
+++ drivesuma.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+
+#start a suma and wait it for 3 seconds
+suma -spec std.141.MNI152_2009_lh.spec -niml &
+sleep 3
+
+echo "\n### load inflated surface"
+
+# DriveSuma -com show_surf -i std.141.lh.inflated.gii \
+#		-label inflated
+DriveSuma -com viewer_cont -key:r2:d period 
+# DriveSuma -com surf_cont -switch_surf std.inflated
+echo "\n### Adapting view..."
+
+# Remove crosshair and axes
+DriveSuma -com viewer_cont -key F3 -key F4 -key F5
+
+# Change viewer size to 400*400 pixels
+DriveSuma -com viewer_cont -viewer_size 1200 1200
+
+# zoom out and have the left lateral view 
+DriveSuma -com viewer_cont -key:r2:d z -key 'Ctrl+left' -key a
+
+# load dataset 
+
+DriveSuma -com surf_cont -load_dset PPI.MVM.lh.niml.dset -surf_label inflated
+
+# generate an array to store the index of the subbricks we need from MVM data
+
+image_dir='PPI_img'
+index=(`seq 4 2 12`)   # only have the beta value subbricks
+subbrickLabel=("Liste_NS" "Listen_CS" "Listen_US" "Listen_SW" "Listen_CS-NS")
+len=${#index[*]} # get the length of the conditions
+
+for ((i=0; i<=$len; i++))
+do
+	image_pre="${subbrickLabel[$i]}"
+	Iindex="${index[$i]}" 			# set the index of intensity subbrick
+	Tindex=$((Iindex + 1 ))			# set the index of t value subbrick
+	
+	# set the prefix of the images you save for each condition
+	export SUMA_AutoRecordPrefix="./${image_dir}/${image_pre}"
+	sleep 5
+	echo $SUMA_AutoRecordPrefix
+	sleep 5
+	DriveSuma -com surf_cont -surf_label inflated \
+		-I_sb $Iindex -T_sb $Tindex -I_range -0.9 \
+			-switch_cmap "Spectrum:red_to_blue+gap"
+	for pose in 'Ctrl+left' 'Ctrl+right' 'Ctrl+down'
+	do
+	# get view and snap
+		DriveSuma -com viewer_cont -key "$pose" -com viewer_cont -key 'Ctrl+r'
+	done
+done
