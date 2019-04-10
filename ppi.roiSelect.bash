fda988e380596648ae4a950fab27070a4b37b1a9
MENGXING <mengluo1990@gmail.com>
2019-04-10 10:50:37 +0200

    change the label

--- ppi.roiSelect.bash
+++ ppi.roiSelect.bash
@@ -3,18 +3,18 @@
 # This script extract ROIs from aparc.a2009 generated from SUMA and Freesurfer
 # and generate seed time course for PPI analysis
 
+# ctx_lh_G_front_inf-Opercular (11112) (220 20 100) (6558940)
+# ctx_lh_G_front_inf-Orbital (11113) (140 60 60) (3947660)
+# ctx_lh_G_front_inf-Triangul (11114) (180 220 140) (9231540)
 # left inferior occipital 11811863
-# ctx-lh-parsopercularis (1018) (220 180 140) (9221340)
-# ctx-lh-parsorbitalis (1019) (20 100 50) (3302420)
-# ctx-lh-parstriangularis (1020) (220 60 20) (1326300)
 # ctx-lh-G_frontal_middle (1109) (140 100 180) (11822220)
 # ctx-lh-G_occipit-temp_lat-Or_fusiform (1117) (60 20 140) (9180220)
 # ctx_lh_G_temporal_middle (11138) (180 60 60) (3947700)
 # ctx_lh_Pole_temporal (11144) (220 180 20) (1357020)
 # ctx_lh_G_pariet_inf-Angular (11125) (20 60 220) (14433300)
 
-index=(11811863 9221340 3302420 \
-		1326300 11822220 9180220 \
+index=(11811863 6558940 3947660 \
+		9231540 11822220 9180220 \
 		3947700 1357020 14433300)
 ROIlabel=("inf_Occipital" "parsOper" "parsOrbi" \
 			"parsTrian" "mid_Frontal" "Fusiform" \
