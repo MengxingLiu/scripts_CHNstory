450ed4f350d44db76f589203b462159fe2971e95
MENGXING <mengluo1990@gmail.com>
2019-07-17 14:47:08 +0200

    import data

---
+++ pyCreatePara.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 17 12:22:10 2019
+
+@author: mengxing
+"""
+
+listenSubs = ['sub02', 'sub03', 'sub04', 'sub05', 'sub06', 'sub07', 'sub08', 
+               'sub09', 'sub12', 'sub13', 'sub15', 'sub16', 'sub31', 'sub32']
+readSubs = ['sub10', 'sub11', 'sub14', 'sub17', 'sub19', 'sub20', 'sub21',
+            'sub22', 'sub23', 'sub24', 'sub25', 'sub26', 'sub27', 'sub28',
+            'sub29' 'sub33']
+conditions = ['CS', 'NS', 'US', 'SW', 'aNS']
---
+++ pyLoadData.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 16 16:01:31 2019
+1, import one subject data
+@author: mengxing
+"""
